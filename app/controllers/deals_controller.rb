25a08ebc33bbe2d8bb3fde5b81df76bf7f7b9627
teguh firman <teguh@41studio.com>
2015-10-19 17:17:39 +0700

    modify index deal page to ajax load

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -25,24 +25,54 @@ class DealsController < ApplicationController
     end
   end
 
+  def create_destination
+    destination = Destination.new()
+    destination.destination_string = params["destinationString"]
+    destination.city = params["city"]
+    destination.state_province_code = params["stateProvinceCode"]
+    destination.country_code = params["countryCode"]
+    destination.latitude = params["lat"]
+    destination.longitude = params["lng"]
+    destination.arrival_date = params["arrivalDate"]
+    destination.departure_date = params["departureDate"]
+    destination.user_id = current_user.id
+    destination.save
+    render json: destination
+  end
+
   private
     def set_search_data
-      if params[:search_deals]
+      @destination =  current_user.destinations.last
+      # if params[:search_deals]
         session[:last_destination_search] = {
-          :latitude => params[:search_deals][:lat],
-          :longitude => params[:search_deals][:lng],
-          :searchRadius => 80,
-          :destinationString => params[:search_deals][:destination].upcase,
-          :city => params[:search_deals][:locality],
-          :stateProvinceCode => params[:search_deals][:administrative_area_level_1],
-          :countryCode => params[:search_deals][:country],
-          :arrivalDate => params[:search_deals][:arrival_date] ,
-          :departureDate => params[:search_deals][:departure_date],
-          :options => 'HOTEL_SUMMARY',
-          :moreResultsAvailable => true,
-          :numberOfResults => 21
-        }
-      end
+            :latitude => @destination.latitude,
+            :longitude => @destination.longitude,
+            :searchRadius => 80,
+            :destinationString => @destination.destination_string.upcase,
+            :city => @destination.city,
+            :stateProvinceCode => @destinationstate_province_code,
+            :countryCode => @destination.country_code,
+            :arrivalDate => @destination.arrival_date.strftime('%m/%d/%Y'),
+            :departureDate => @destination.departure_date.strftime('%m/%d/%Y'),
+            :options => 'HOTEL_SUMMARY,ROOM_RATE_DETAILS',
+            :moreResultsAvailable => true,
+            :numberOfResults => 21
+          }
+        # session[:last_destination_search] = {
+        #   :latitude => params[:search_deals][:lat],
+        #   :longitude => params[:search_deals][:lng],
+        #   :searchRadius => 80,
+        #   :destinationString => params[:search_deals][:destination].upcase,
+        #   :city => params[:search_deals][:locality],
+        #   :stateProvinceCode => params[:search_deals][:administrative_area_level_1],
+        #   :countryCode => params[:search_deals][:country],
+        #   :arrivalDate => params[:search_deals][:arrival_date] ,
+        #   :departureDate => params[:search_deals][:departure_date],
+        #   :options => 'HOTEL_SUMMARY,ROOM_RATE_DETAILS',
+        #   :moreResultsAvailable => true,
+        #   :numberOfResults => 21
+        # }
+      # end
       set_hotel("get_list", session[:last_destination_search])
     end
 
@@ -53,10 +83,9 @@ class DealsController < ApplicationController
           if event.eql? "get_list"
             api.get_list(params)
           elsif event.eql? "get_information"
-            # api.get_information(params)
-            api.get_information({hotelId: 356564, options: "HOTEL_SUMMARY,HOTEL_DETAILS,HOTEL_IMAGES"})
+            api.get_information(params)
+            # api.get_information({hotelId: 356564, options: "HOTEL_SUMMARY,HOTEL_DETAILS,HOTEL_IMAGES"})
           end
-
         response.exception?
         if response.exception?.eql? true
           @error_response = response.presentation_message
@@ -68,13 +97,14 @@ class DealsController < ApplicationController
             elsif event.eql? "get_information"
               response.body["HotelInformationResponse"]
             end
+          # yuhuu
 
           if event.eql? "get_list"
             @hotel_ids = @hotels_list.map { |hotel| hotel["hotelId"] }
             @like_ids = Like.where(hotel_id: @hotel_ids, user_id: current_user.id).pluck(:hotel_id)
 
-            session[:hotel_list_cache_key] = response.body["HotelListResponse"]["cacheKey"]
-            session[:hotel_list_cache_location] = response.body["HotelListResponse"]["cacheLocation"]
+            @hotel_list_cache_key = response.body["HotelListResponse"]["cacheKey"]
+            @hotel_list_cache_location = response.body["HotelListResponse"]["cacheLocation"]
           end
 
         end
@@ -84,5 +114,9 @@ class DealsController < ApplicationController
     def check_like
       @like = Like.find_by(hotel_id: params[:id], user_id: current_user.id)
     end
+
+    # def destination_params
+    #   params.require(:destination).permit(:destination_string, :city, :state_province_code, :country_code, :latitude, :longitude, :arrival_date, :departure_date)
+    # end
 
 end
