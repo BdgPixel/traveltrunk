137d21db6752a6dbfde39354befa827e481bf13c
teguh firman <teguh@41studio.com>
2015-10-30 17:17:20 +0700

    assets precompile

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -1,7 +1,8 @@
 class SavingsController < ApplicationController
   def index
     if current_user.group
-      @members = current_user.group.members
+      @members = current_user.group.members.select(:id, :email)
+      # @joined_members = @members.map { |u| { id: u.id, name: u.email } }
     end
     @joined_groups = current_user.joined_groups
     # yuhuu
