cb6771355dc6f91b11a3c64e941d6717eef77ace
teguh firman <teguh@41studio.com>
2015-11-04 16:45:25 +0700

    correct groups name to no name for groups saving

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,4 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
-
-  # GET /groups
-  # GET /groups.json
-  def index
-    @groups = Group.all
-  end
-
-  # GET /groups/1
-  # GET /groups/1.json
-  def show
-    @joined_members = @group.members.select(:id, :email).map { |u| { id: u.id, name: u.email } }
-  end
-
   def users_collection
     users_list = User.get_autocomplete_data(params[:q], current_user.id)
 
@@ -25,7 +11,7 @@ class GroupsController < ApplicationController
     group = current_user.group
 
     unless group
-      group = Group.create(name: "#{current_user.profile.first_name}'s Savings Group", user_id: current_user.id)
+      group = Group.create(name: "Your Group's Savings", user_id: current_user.id)
     end
 
     group_hashs = params[:invite][:user_id].split(',').uniq
@@ -51,66 +37,4 @@ class GroupsController < ApplicationController
     end
 
   end
-
-  # GET /groups/new
-  def new
-    @group = Group.new
-  end
-
-  # GET /groups/1/edit
-  def edit
-  end
-
-  # POST /groups
-  # POST /groups.json
-  def create
-    @group = Group.new(group_params)
-    @group.user_id = current_user.id
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /groups/1
-  # PATCH/PUT /groups/1.json
-  def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /groups/1
-  # DELETE /groups/1.json
-  def destroy
-    @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def group_params
-      params[:group]
-      params.require(:group).permit(:name, :user_id)
-    end
 end
