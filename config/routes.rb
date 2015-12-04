d39b8ab6f044abe1130a5da629340fd58377167d
teguh firman <teguh@41studio.com>
2015-12-04 14:32:19 +0700

    add form modal "add to savings"

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get  'payments/thank_you_page'
   post 'payments/stripe_webhook'
 
-  get 'savings' => 'savings#index'
+  get  'savings' => 'savings#index'
 
   get 'notifications' => 'notifications#index'
 
@@ -20,18 +20,20 @@ Rails.application.routes.draw do
     end
   end
 
-  get  'deals' => 'deals#index'
-  get  'deals/:id/show' => 'deals#show', as: 'deals/show'
-  get  'deals/:id/like' => 'deals#like', as: 'deals/like'
-  get  'deals/next' => 'deals/next'
-  get  'deals/previous' => 'deals/previous'
-  get  'deals/:id/room_availability' => 'deals#room_availability', as: 'deals/room_availability'
-  get  'deals/:id/room_images' => 'deals#room_images', as: 'deals/room_images'
-  get  'deals/book' => 'deals#book', as: 'deals/book'
-  get  'deals/thank_you_page'
-  post 'deals/create_book' => 'deals#create_book', as: 'deals/create_book'
-  post 'deals/search' => 'deals#search'
-  post 'deals/create_destination' => 'deals/create_destination'
+  get   'deals' => 'deals#index'
+  get   'deals/:id/show' => 'deals#show', as: 'deals/show'
+  get   'deals/:id/like' => 'deals#like', as: 'deals/like'
+  get   'deals/next' => 'deals/next'
+  get   'deals/previous' => 'deals/previous'
+  get   'deals/:id/room_availability' => 'deals#room_availability', as: 'deals/room_availability'
+  get   'deals/:id/room_images' => 'deals#room_images', as: 'deals/room_images'
+  get   'deals/book' => 'deals#book', as: 'deals/book'
+  get   'deals/thank_you_page'
+  post  'deals/create_book' => 'deals#create_book', as: 'deals/create_book'
+  post  'deals/search' => 'deals#search'
+  post  'deals/create_destination' => 'deals/create_destination'
+  # patch 'deals/update_credit' => 'deals/update_credit'
+  put   'deals/update_credit'
 
   devise_for :users
 
