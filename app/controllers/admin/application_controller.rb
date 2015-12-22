68572900a31a808b7693fcee234bca5799ff2016
teguh firman <teguh@41studio.com>
2015-12-22 21:15:26 +0700

    redirect to root path if user not sign in

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,11 +2,16 @@ class Admin::ApplicationController < ActionController::Base
   layout 'application'
 
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :authenticate_admin!
 
   def authenticate_admin!
-    unless current_user.admin?
-      redirect_to savings_url
+    if user_signed_in?
+      unless current_user.admin?
+        redirect_to savings_url
+      end
+    else
+      root_url
     end
   end
 
