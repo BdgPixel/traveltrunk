dda7011aa60cfc813d21ae34df8cc97545c084df
teguh firman <teguh@41studio.com>
2015-12-28 16:53:47 +0700

    bug fix invalid token promo code

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -17,25 +17,31 @@ class PromoCodesController < ApplicationController
       @promo_code = PromoCode.find_by(user_id: current_user.id, token: params[:promo_code][:token])
 
       respond_to do |format|
-        if @promo_code.is_expired?
-          flash[:error] = "Your token is expired"
-          format.js
-          format.html { redirect_to promo_codes_activation_url, notice: flash[:error] }
-        else
-          if @promo_code.status.eql?('available')
-            @promo_code.update_attributes(status: 'used')
-
-            current_user.total_credit_in_cents(@promo_code.amount)
-            current_user.save
-
-            flash[:notice] = 'Your token promo code has been activated'
-            format.js { render js: "window.location.href='#{savings_url}'", notice: flash[:notice]   }
-            format.html { redirect_to promo_codes_activation_url, notice: flash[:notice] }
+        if @promo_code
+          if @promo_code.is_expired?
+            flash[:error] = "Your token is expired"
+            format.js
+            format.html { redirect_to promo_codes_activation_url, notice: flash[:error] }
           else
-            flash[:error] = 'Promo code already used'
-            format.js {}
-            format.html { redirect_to promo_codes_activation_url }
+            if @promo_code.status.eql?('available')
+              @promo_code.update_attributes(status: 'used')
+
+              current_user.total_credit_in_cents(@promo_code.amount)
+              current_user.save
+
+              flash[:notice] = 'Your token promo code has been activated'
+              format.js { render js: "window.location.href='#{savings_url}'", notice: flash[:notice]   }
+              format.html { redirect_to promo_codes_activation_url, notice: flash[:notice] }
+            else
+              flash[:error] = 'Promo code already used'
+              format.js {}
+              format.html { redirect_to promo_codes_activation_url }
+            end
           end
+        else
+          flash[:error] = 'Invalid token promo code'
+          format.js {}
+          format.html { redirect_to promo_codes_activation_url }
         end
       end
 
