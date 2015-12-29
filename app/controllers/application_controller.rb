393f04add68c057048b8e6b091ef94689a933066
teguh firman <teguh@41studio.com>
2015-12-29 09:57:57 +0700

    change http basic authenticate to method in application  and skip before action into only accessing webhook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,29 @@
 class ApplicationController < ActionController::Base
   hide_action :current_user
   before_action :get_unread_notification_count
+  before_action :authenticate
   # http_basic_authenticate_with name: "traveltrunk", password: "A8BzR2YKnguxWZz"
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    if resource.admin?
-     admin_promo_codes_url
-    else
-      root_url
+  protected
+    def after_sign_in_path_for(resource)
+      if resource.admin?
+       admin_promo_codes_url
+      else
+        root_url
+      end
     end
-  end
 
-  def get_unread_notification_count
-    @notification_count = current_user.get_notification(false).count if user_signed_in?
-  end
+    def get_unread_notification_count
+      @notification_count = current_user.get_notification(false).count if user_signed_in?
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "traveltrunk" && password == "A8BzR2YKnguxWZz"
+      end
+    end
 end
