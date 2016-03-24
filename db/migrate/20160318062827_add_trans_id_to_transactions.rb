62732456c18187f9a3c74e01e53eb395838c7498
teguh41studio <teguh@41studio.com>
2016-03-24 18:17:02 +0700

    comment column ref_id transaction in migration

--- db/migrate/20160318062827_add_trans_id_to_transactions.rb
+++ db/migrate/20160318062827_add_trans_id_to_transactions.rb
@@ -1,6 +1,6 @@
 class AddTransIdToTransactions < ActiveRecord::Migration
   def change
-    add_column :transactions, :ref_id, :string
+    # add_column :transactions, :ref_id, :string
     add_column :transactions, :trans_id, :string
   end
 end
