a4e1c5d6ada08ff444e8609441eeeacc975fb460
teguh41studio <teguh@41studio.com>
2016-04-04 10:23:08 +0700

    remove unused filter parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :creditCard, :cvc, :card_number]
+Rails.application.config.filter_parameters += [:password, :cvc, :credit_card]
