1b9217a9fee544c4c6296d02874adcf6402d56da
teguh41studio <teguh@41studio.com>
2016-04-04 10:29:23 +0700

    change active record "all" to "order" in admin transactions page

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.all.page params[:page]
+    @transactions = Transaction.order(created_at: :desc).page params[:page]
   end
 end
