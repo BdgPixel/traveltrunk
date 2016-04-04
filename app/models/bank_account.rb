f8884eeef851b517f54928459a0f26d7b5b66d08
teguh41studio <teguh@41studio.com>
2016-04-04 10:51:31 +0700

    refactoring bank account model

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -26,10 +26,11 @@ class BankAccount < ActiveRecord::Base
     user = self.user
 
     if user
-      begin
-        customer_from_authorize_net = AuthorizeNetLib::Customers.new
-        unless user.customer
-          merchant_customer_id = AuthorizeNetLib::Global.genrate_random_id('cus')
+      unless user.customer
+        begin
+          customer_from_authorize_net = AuthorizeNetLib::Customers.new
+          merchant_customer_id = AuthorizeNetLib::Global.generate_random_id('cus')
+
           response = customer_from_authorize_net.create_profile({ 
             merchant_customer_id: merchant_customer_id, 
             email: self.user.email 
@@ -38,11 +39,12 @@ class BankAccount < ActiveRecord::Base
           if response.messages.resultCode.eql? 'Ok'
             Customer.create(customer_id: merchant_customer_id, user_id: user.id, customer_profile_id: response.customerProfileId)
           end
+        rescue Exception => e
+          logger.error e.message
+
+          self.errors.add(:authorize_net_error, e.error_message[:response_message])
+          false
         end
-      rescue Exception => e
-        logger.error e.message
-        self.errors.add(:authorize_net_error, e.error_message[:response_message])
-        false
       end
     end
   end
@@ -51,23 +53,16 @@ class BankAccount < ActiveRecord::Base
     user = self.user
     
     if user
-      customer = Customer.where(user_id: user.id).first
-      customer_profile_id = customer.customer_profile_id
+      customer = Customer.where(user_id: user).first
 
       user_subscription = user.subscription
       amount = self.amount_transfer.to_f
       interval_frequency, interval_count = self.transfer_type
       plan_name = "#{user.profile.first_name.titleize} #{self.transfer_frequency} Savings Plan"
-      subscription_id = user_subscription.subscription_id if user_subscription
-
-      recurring_authorize = AuthorizeNetLib::RecurringBilling.new
-      customer_authorize = AuthorizeNetLib::Customers.new
 
       begin
         params_recurring = {
-          subscription_id: subscription_id,
-          ref_id: AuthorizeNetLib::Global.genrate_random_id('ref'),
-          user_id: user.id,
+          ref_id: AuthorizeNetLib::Global.generate_random_id('ref'),
           card: {
             credit_card: self.credit_card,
             cvc: self.cvc,
@@ -76,13 +71,11 @@ class BankAccount < ActiveRecord::Base
           plan: {
             interval_unit: interval_frequency,
             interval_length: interval_count,
-            star_date: (DateTime.now).to_s[0...10],
-            total_occurrences: '9999',
-            amount: amount.to_f,
+            trial_occurrences: '0',
+            amount: amount,
             plan_name: plan_name
           },
           customer: {
-            # name: "#{self.user.profile.first_name} #{self.user.profile.last_name}",
             customer_id: customer.customer_id,
             first_name: user.profile.first_name,
             last_name: user.profile.last_name,
@@ -92,86 +85,78 @@ class BankAccount < ActiveRecord::Base
             city: user.profile.city,
             state: user.profile.state,
             zip: user.profile.postal_code,
-            country: user.profile.country_code
+            country: user.profile.country_code,
           },
           order: {
-            invoice_number: AuthorizeNetLib::Global.genrate_random_id('inv'),
-            description: 'Recurring Subscription'
+            invoice_number: AuthorizeNetLib::Global.generate_random_id('inv') 
           },
         }
 
-        get_customer = customer_authorize.get_customer_profile(customer_profile_id)
-        customer_profile_id = get_customer.profile.customerProfileId
-        payment_profile_id = get_customer.profile.paymentProfiles.first.customerPaymentProfileId if user_subscription
+        customer_authorize = AuthorizeNetLib::Customers.new
+        customer_profile = customer_authorize.get_customer_profile(customer.customer_profile_id)
+        customer_payment_profile = customer_profile.paymentProfiles.first
+      
+        recurring_authorize = AuthorizeNetLib::RecurringBilling.new
 
-        last_card_number = 
-          if user_subscription
-            if get_customer.profile.paymentProfiles.first.payment.creditCard.cardNumber[-4..-1].eql?(self.credit_card[-4..-1])
-              true
-            else
-              false
-            end
-          end
+        if user_subscription
+          customer_credit_card_last_4 = customer_payment_profile.payment.creditCard.cardNumber.last(4) rescue nil
+          credit_card_changed = customer_credit_card_last_4 != self.credit_card.last(4)
+        
+          if credit_card_changed || self.changed.include?('amount_transfer') || self.changed.include?('transfer_frequency')
+            selected_params = params_recurring.select { |k, v| [:subscription_id, :plan].include?(k) }
+            subscription_hash = user_subscription.get_params_hash(selected_params)
+            # binding.pry
+            params_recurring[:plan][:trial_occurrences] = '1'
+            subscription_response = recurring_authorize.create_subscription(params_recurring)
 
-        if user_subscription.nil? || self.changed.include?('amount_transfer') || self.changed.include?('transfer_frequency')
-          if user_subscription
-            if last_card_number
-              update_subscription = recurring_authorize.update_subscription(subscription_id, params_recurring[:plan][:amount])
-
-              if update_subscription.messages.resultCode.eql? 'Ok'
-                selected_params = params_recurring.select { |k, v| [:subscription_id, :plan, :user_id].include?(k) }
-                subscription_hash = user_subscription.params_hash(selected_params)
-                user_subscription.update_attributes(subscription_hash)
-
+            if subscription_response.messages.resultCode.eql? 'Ok'
+              if customer_payment_profile
+                response = recurring_authorize.cancel_subscription(user_subscription.subscription_id, customer_profile.customerProfileId, customer_payment_profile.customerPaymentProfileId)
+                response.messages.resultCode
               end
-            else
-              cancel_subscription = recurring_authorize.cancel_subscription(subscription_id, customer_profile_id, payment_profile_id)
 
-              if cancel_subscription.messages.resultCode.eql? 'Ok'
-                create_subscription = recurring_authorize.create_subscription(params_recurring)
-                user_subscription.update(subscription_id: create_subscription.subscriptionId)
-              end
+              subscription_hash.merge!(subscription_id: subscription_response.subscriptionId)
+              user_subscription.update(subscription_hash)
             end
 
-            PaymentProcessorMailer.subscription_created(user.id).deliver_now
-          else
-            create_subscription = recurring_authorize.create_subscription(params_recurring)
-
-            if create_subscription.messages.resultCode.eql? 'Ok'
-              Subscription.create({
-                subscription_id: create_subscription.subscriptionId,
-                amount: amount * 100,
-                interval: interval_frequency,
-                interval_count: interval_count,
-                plan_name: plan_name,
-                user_id: user.id
-              })
-            end
-            
-            PaymentProcessorMailer.subscription_created(user.id).deliver_now
-          end
+          end 
         else
-          unless last_card_number
-            cancel_subscription = recurring_authorize.cancel_subscription(subscription_id, customer_profile_id, payment_profile_id)
+          # create subscription
+          response = recurring_authorize.create_subscription(params_recurring)
 
-            if cancel_subscription.messages.resultCode.eql? 'Ok'
-              create_subscription = recurring_authorize.create_subscription(params_recurring)
-              user_subscription.update(subscription_id: create_subscription.subscriptionId)
-            end
+          if response.messages.resultCode.eql? 'Ok'
+            Subscription.create({
+              subscription_id: response.subscriptionId,
+              amount: amount * 100,
+              interval: interval_frequency,
+              interval_count: interval_count,
+              plan_name: plan_name,
+              user_id: user.id
+            })
           end
+          
         end
 
+        # PaymentProcessorMailer.subscription_created(user.id).deliver_now
       rescue Exception => e
         logger.error e.message
-        response_message = 
-          if e.error_message[:response_error_code].eql?('E00003')
-            e.message
-          else
-            e.error_message[:response_message]
-          end
 
-        self.errors.add(:authorize_net_error, response_message)
-        false
+        if e.is_a?(AuthorizeNetLib::RescueErrorsResponse)
+          @error_response = 
+            if e.error_message[:response_error_text]
+              "#{e.error_message[:response_message]} #{e.error_message[:response_error_text]}"
+            else
+              puts e.error_message
+              puts e.message
+              e.error_message[:response_message].split('-').last.strip
+            end
+            
+          self.errors.add(:authorize_net_error, @error_response)
+          false
+        else
+          logger.error e.message
+          e.backtrace.each { |line| logger.error line }
+        end
       end
     end
   end
