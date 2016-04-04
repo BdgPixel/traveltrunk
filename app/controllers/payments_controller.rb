59fc34bfdcfe2d70e5d6afd87b7113895bce63d3
teguh41studio <teguh@41studio.com>
2016-04-04 13:46:42 +0700

    remove comment code

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,6 @@ class PaymentsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:authorize_net_webhook]
   skip_before_action :authenticate, only: :authorize_net_webhook
   before_action :authenticate_user!, only: [:create, :thank_you_page]
-  # before_action :set_transaction, only: :authorize_net_webhook
 
   def create
     if !params[:payment][:amount].empty?
@@ -74,77 +73,8 @@ class PaymentsController < ApplicationController
       end
     end
 
-    # # @transaction = Transaction.where(user_id, amount: , customer_id, tra)
-
-    # if @transaction
-    #     @transaction.user_id = user_id
-    #     # @transaction.invoice_id = response['x_invoice_num']
-    #     @transaction.amount = response['x_amount'].to_f * 100
-    #     @transaction.customer_id = response['x_cust_id']
-    #     @transaction.transaction_type = 'deposit'
-    #     @transaction.trans_id = response['x_trans_id']
-    # else
-    #   @transaction = Transaction.new(
-    #     user_id: user_id,
-    #     invoice_id: response['x_invoice_num'],
-    #     amount: response['x_amount'].to_f * 100,
-    #     customer_id: response['x_cust_id'],
-    #     transaction_type: 'deposit',
-    #     trans_id: response['x_trans_id']
-    #   )
-    # end
-
-    # # binding.pry
-    # if @transaction.save
-    #   if @transaction.user_id
-    #     user = User.find(@transaction.user_id)
-    #     user.total_credit += @transaction.amount
-    #     user.save
-
-    #     StripeMailer.subscription_charged(user.id, @transaction.amount).deliver_now
-    #     user.create_activity key: "payment.recurring", owner: user,
-    #       recipient: user, parameters: { amount: (@transaction.amount / 100.0), total_credit: user.total_credit / 100.0 }
-    #   end
-    # end
-
     render nothing: true, status: 200
   end
-
-=begin
-  def stripe_webhook
-    response = JSON.parse(request.body.read)
-
-    if response['type'].eql?('invoice.payment_succeeded') && response['data']['object']['amount_due'] > 0
-      response = response['data']['object']
-
-      transaction = Transaction.new(
-        user_id: response['lines']['data'].last['metadata']['user_id'],
-        invoice_id: response['id'],
-        amount: response['amount_due'],
-        customer_id: response['customer'],
-        transaction_type: 'deposit'
-      )
-
-      if transaction.save
-        user = User.find(transaction.user_id)
-        user.total_credit += transaction.amount
-        user.save
-
-        StripeMailer.subscription_charged(user.id, transaction.amount).deliver_now
-        user.create_activity key: "payment.recurring", owner: user,
-          recipient: user, parameters: { amount: (transaction.amount / 100.0), total_credit: user.total_credit / 100.0 }
-      end
-
-    end
-
-    render nothing: true, status: 200
-  end
-=end
-
-  # private
-  #   def set_transaction
-  #     @transaction = Transaction.find_by(trans_id: params['x_trans_id'])
-  #   end
 
 end
 
