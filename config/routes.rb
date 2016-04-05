6635c4eb9d79f5f2209484d74745fff92dd6099f
teguh41studio <teguh@41studio.com>
2016-04-05 16:17:35 +0700

    add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  get 'refunds/index'
-  end
+  get 'privacy_policy' => 'policies#privacy'
+  get 'refund_policy' => 'policies#refund'
 
+  get  'contact_us' => 'contacts#index'
+  post 'contact_us' => 'contacts#create'
+  
   get  'refunds/create'
 
   get  'promo_codes/activation'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321043225) do
+ActiveRecord::Schema.define(version: 20160405035115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 20160321043225) do
   end
 
   add_index "bank_accounts", ["user_id"], name: "index_bank_accounts_on_user_id", using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "subject"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "customer_id"
