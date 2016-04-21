b18a1b1df67e0215bb01ba2df7b1d782d0c9046d
teguh41studio <teguh@41studio.com>
2016-04-21 14:56:47 +0700

    add method class get_reservation_list in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,10 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
+
+  paginates_per 10
+
+  def self.get_reservation_list
+    self.select(:id, :user_id, :hotel_name, :city, :total, :status, :created_at).order(created_at: :desc)
+  end
+
 end
