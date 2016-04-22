ebebb1f01dc3a723c0b157963d5343e3b1821b56
teguh41studio <teguh@41studio.com>
2016-04-22 15:26:43 +0700

    add new columns migration reservation_type and status to reservations

---
+++ db/migrate/20160422082426_add_columns_to_reservations.rb
@@ -0,0 +1,6 @@
+class AddColumnsToReservations < ActiveRecord::Migration
+  def change
+    add_column :reservations, :reservation_type, :string, default: 'person'
+    add_column :reservations, :status, :string, default: 'reserved'
+  end
+end
