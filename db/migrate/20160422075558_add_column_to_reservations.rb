a2b4440e0b48da82ecca0fcb5ecad9d85fd18a92
teguh41studio <teguh@41studio.com>
2016-04-22 15:26:00 +0700

    deleted old migrations

--- db/migrate/20160422075558_add_column_to_reservations.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToReservations < ActiveRecord::Migration
-  def change
-    add_column :reservations, :reservation_type, :string, default: 'person'
-    add_column :reservations, :status, :string, default: 'reserved'
-  end
-end
