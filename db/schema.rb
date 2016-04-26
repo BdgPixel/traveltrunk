be332d4df94921ad540e85b39de7a2c9216a6947
teguh41studio <teguh@41studio.com>
2016-04-26 10:39:07 +0700

    add total saving in detail user for admin page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422075558) do
+ActiveRecord::Schema.define(version: 20160422082426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
