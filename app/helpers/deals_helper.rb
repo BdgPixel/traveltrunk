53a48e9db38722f53a6fb1024013f9316266d39a
teguh41studio <teguh@41studio.com>
2016-04-27 09:18:46 +0700

    rescue if hotel image equal to nil in deals helper

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -10,8 +10,10 @@ module DealsHelper
   end
 
   def list_of_deals_div(hotel_image)
-    content_tag(:div, nil, class: 'lazy deals-image', data: { original: "http://images.travelnow.com#{ hotel_image.gsub('_t.', '_y.') }" },
-      style: "background:url('http://images.travelnow.com#{ hotel_image.gsub('_t.', '_b.') }'), url('http://images.travelnow.com#{ hotel_image.gsub('_t.', '_l.') }') no-repeat grey; background-size: 100% 100%; height: 300px;")
+    if hotel_image
+      content_tag(:div, nil, class: 'lazy deals-image', data: { original: "http://images.travelnow.com#{ hotel_image.gsub('_t.', '_y.') }" },
+        style: "background:url('http://images.travelnow.com#{ hotel_image.gsub('_t.', '_b.') }'), url('http://images.travelnow.com#{ hotel_image.gsub('_t.', '_l.') }') no-repeat grey; background-size: 100% 100%; height: 300px;")
+    end
   end
 
   def welcome_user_first_sign_in
