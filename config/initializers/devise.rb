983af8e8b1dcfe9680bf9ae1312f7afd0b72b508
dian septiana <dian@41studio.com>
2016-06-03 10:22:58 +0700

    Add secret_key to devise initializer, add staging environment in secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '6bd7e2fec1a4aa2025b17d00b6b9cd79232a3abea0946645a0a35b8b09f85ffe1840b9d2b50fc3225de36f7d7a5c228b6f0915675f48356bad28a6a66d1da45f'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
