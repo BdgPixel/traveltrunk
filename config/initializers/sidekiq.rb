00d3f04789d9346405be4be00378a3ae54ddb10c
dian septiana <dian@41studio.com>
2016-06-03 14:39:03 +0700

    Add namespace to redis for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,9 @@
 Sidekiq.default_worker_options['retry'] = 2
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_travel_trunk_#{Rails.env}" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_travel_trunk_#{Rails.env}" }
+end     
