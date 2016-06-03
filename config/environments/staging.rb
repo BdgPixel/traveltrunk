664d33150002a16009a4e0eb3adea0a60bcb7907
dian septiana <dian@41studio.com>
2016-06-03 15:10:52 +0700

    Change domain in smtp settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:        ENV['MAILGUN_SMTP_SERVER'],
     port:           ENV['MAILGUN_SMTP_PORT'],
-    domain:         'traveltrunk.herokuapp.com',
+    domain:         'staging.traveltrunk.us',
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
     authentication: :plain
