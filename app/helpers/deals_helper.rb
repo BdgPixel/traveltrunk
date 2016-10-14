53ae6d3ed9d4d64e272f91a47a6d154ff65e0970
teguh41studio <teguh@41studio.com>
2016-10-14 17:49:47 +0700

    Change styling table in confirmation email

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -2,9 +2,17 @@ module DealsHelper
   include ActionView::Helpers::UrlHelper
 
   COLLECTION_CARDS = [
-    ['Discover', 'DC'],
+    ['American Express', 'AX'],
+    ['BC Card', 'BC'],
+    ['Carta Si', 'T'],
+    ['Carte Bleue', 'R'],
+    ['Diners Club', 'DC'],
+    ['Discover', 'DS'],
+    ['Japan Credit Bureau', 'JC'],
+    ['Maestro', 'TO'],
     ['Master Card', 'CA'],
     ['Visa', 'VI'],
+    ['Visa Electron', 'E'],
   ]
 
   def unescape_expedia_html(string)
@@ -70,7 +78,7 @@ module DealsHelper
 
     if cost['@size'].to_i > 1
       cost['Surcharge'].each do |surcharge|
-        tags += "<tr><td class='m_2076496084396982010border-right' style='border-right-width:1px;border-right-style:solid'>"
+        tags += "<tr><td class='border-right' style='border-right-width:1px;border-right-style:solid;border-color: #e2e2e2'>"
           tags += "<font style='font-family:Tahoma,sans-serif;font-size:13px'>"
 
           if surcharge['@type'].eql? 'SalesTax'
@@ -89,7 +97,7 @@ module DealsHelper
         tags +="</td></tr>"
       end
     else
-      tags += "<tr><td class='m_2076496084396982010border-right' style='border-right-width:1px;border-right-style:solid'>"
+      tags += "<tr><td class='border-right' style='border-right-width:1px;border-right-style:solid;border-color: #e2e2e2'>"
         tags += "<font style='font-family:Tahoma,sans-serif;font-size:13px'>"
           
           if cost['@type'].eql? 'SalesTax'
