9d835aa7915f2c7ffc01b05fa39ca001d59672b9
teguh41studio <teguh@41studio.com>
2016-10-20 10:41:44 +0700

    Fix navbar transparent and white when scrolling, and move code slider group to group.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -19,33 +19,35 @@ var initParalax = function() {
 };
 
 var initScrolling = function() {
-  var scroll = $(window).scrollTop();
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
 
-  if (scroll >= 10) {
-      $(".transparent").addClass("scrolling");
-      $("#logo-color-orange").removeClass("hide");
+    if (scroll >= 10) {
+        $(".transparent").addClass("scrolling");
+        $("#logo-color-orange").removeClass("hide");
 
-      $("#logo-color-white").addClass("hide");
-      $("#logo-color-white").removeClass("");
+        $("#logo-color-white").addClass("hide");
+        $("#logo-color-white").removeClass("");
 
-      $(".btn-border").addClass("hide");
-      $(".btn-border").removeClass("show");
+        $(".btn-border").addClass("hide");
+        $(".btn-border").removeClass("show");
 
-      $(".btn-orange2").removeClass("hide");
-      $(".link-top-login").addClass('grey-nav-color');
-  } else {
-      $(".transparent").removeClass("scrolling");
-      $("#logo-color-white").removeClass("hide");
-      $("#logo-color-white").addClass("");
+        $(".btn-orange2").removeClass("hide");
+        $(".link-top-login").addClass('grey-nav-color');
+    } else {
+        $(".transparent").removeClass("scrolling");
+        $("#logo-color-white").removeClass("hide");
+        $("#logo-color-white").addClass("");
 
-      $("#logo-color-orange").addClass("hide");
+        $("#logo-color-orange").addClass("hide");
 
-      $(".btn-border").removeClass("show");
-      $(".btn-border").addClass("show");
+        $(".btn-border").removeClass("show");
+        $(".btn-border").addClass("show");
 
-      $(".btn-orange2").addClass("hide");
-      $(".link-top-login").removeClass('grey-nav-color');
-  }
+        $(".btn-orange2").addClass("hide");
+        $(".link-top-login").removeClass('grey-nav-color');
+    }
+  });
 };
 
 var initToolTip = function() {
@@ -61,70 +63,14 @@ var initToolTip = function() {
   });
 };
 
-var initSwipeGroupSaving = function() {
-  $('.swipe-group-saving').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    prevArrow: false,
-    nextArrow: false,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 960,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          variableWidth: false,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          variableWidth: false,
-          dots: false
-        }
-      },
-
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          variableWidth: false,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          variableWidth: false
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-};
-
 var ready = function() {
   initParalax();
   initScrolling();
   initToolTip();
-  initSwipeGroupSaving();
 };
 
 $(document).ready(function() {
-  ready()
+  ready();
 });
 
 $(document).on('page:load', function() {
