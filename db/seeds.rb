daf8514b431da73aa5e6f01ba3e678675de6acdb
dian41studio <dian@41studio.com>
2016-10-27 10:26:07 +0700

    Fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ puts 'Creating admin'
 admin_users_count = User.where(admin: true).count
 
 if admin_users_count.zero?
-  user = User.new(email: 'admin@traveltrunk.us', password: ENV['ADMIN_PASSWORD'],
-    password_confirmation: ENV['ADMIN_PASSWORD'], admin: true)
-  
-  user.confirm
-  
+  password = ENV['ADMIN_PASSWORD'] || '12345678'
+
+  user = User.new(email: 'admin@traveltrunk.us', password: password,
+    password_confirmation: password, admin: true)
+    
   if user.save
-    puts "Admin user created: #{user.to_json}"
+    puts "Admin user created"
   else
     puts "Error creating admin user: #{user.errors.full_messages}"
   end
