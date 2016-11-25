30c4072f54697118486fedd30696670f3194dd9d
teguh <teguh@41studio.com>
2016-11-25 17:30:56 +0700

    Fix nil class

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -251,6 +251,6 @@ module DealsHelper
   end
 
   def init_title_destination(destination)
-    destination ? destination.title_destination.html_safe : 'Where would you like to be?'
+    destination.present? ? destination.title_destination.html_safe : 'Where would you like to be?'
   end
 end
